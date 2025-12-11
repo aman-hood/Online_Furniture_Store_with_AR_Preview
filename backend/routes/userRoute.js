3f78b2d3cd4ab8b6db3d11b07e7b54a63b000e60
nancy-jaiswal19 <nancyjais1907@gmail.com>
2025-12-11 23:15:50 +0530

    Add Categories Pages

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,7 +3,12 @@ import {register, verify, reVerify, login, logout, forgotPassword, verifyOTP, ch
 import { isAdmin, isAuthenticated } from '../middlewares/isAuthenticated.js';
 const router = express.Router()
 
-
+router.get("/me", isAuthenticated, (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+});
 // router.get('/verify', verify);
 console.log("User routes loaded!");
 
